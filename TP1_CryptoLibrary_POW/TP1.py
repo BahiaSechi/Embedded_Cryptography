cf4bd934ba8287f8419deff4e5e5ab763df33902
SECHI Bahia <bahia.sechi@ecole.ensicaen.fr>
2020-11-27 16:33:35 +0100

    Question 1 finished beginnning of question 2

--- TP1_CryptoLibrary_POW/TP1.py
+++ TP1_CryptoLibrary_POW/TP1.py
@@ -1,24 +1,42 @@
 import time
+
+import Crypto
 from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
 
 temps0 = time.perf_counter()
 
-# Exercice 1
+# Question 1
+
+print("Question 1")
 
 m = SHA256.new()
-m.update(b"debut")
-m.update(b"suite")
-print(m.hexdigest())
 
-n = SHA256.new(data=b"debutsuite")
-print(n.hexdigest())
+n = 0
+
+while not m.hexdigest().endswith("4c4f4c"):
+    m.update(b"bahia")
+    n += 1
+    print(m.hexdigest())
+
+print("n = " + str(n) + " \nhexdigest = " + m.hexdigest())
 
 temps1 = time.perf_counter()
-print('Temps exercice 1 :', temps1-temps0, 'secondes')
+print('Question 1 time :', temps1-temps0, 'seconds')
 
-# Exercice 2
+e = SHA256.new(n*b"bahia")
+print("Verification:" + str(e.digest()))
 
-# INSTRUCTIONS
+# Question 2
 
-# temps2 = time.perf_counter()
-# print ('Temps2 :', temps2-temps1, 'secondes')
+print("Question 2")
+
+chiffrement = AES.new(b"Sixteen byte key", AES.MODE_ECB)
+m_chiffre = chiffrement.encrypt(b"message_128nbits")
+print(chiffrement.decrypt(m_chiffre).decode("utf_8"))
+
+rand_nb = Crypto.Random.get_random_bytes(128)
+print(rand_nb)
+
+temps2 = time.perf_counter()
+print('Question 2 time :', temps2-temps1, 'seconds')
