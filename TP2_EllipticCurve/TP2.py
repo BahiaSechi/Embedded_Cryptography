7df06f2c3d85f0e95c01ee43c30697244f7e92ae
Bastien Prunier <bprunier@ecole.ensicaen.fr>
2020-12-02 11:14:44 +0100

    debut de verifier_point

--- TP2_EllipticCurve/TP2.py
+++ TP2_EllipticCurve/TP2.py
@@ -2,6 +2,19 @@
 # PRUNIER Bastien
 # SECHI Bahia
 
-def verifie_point(A,B,p,P):
-    print("hey")
+import math  
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def verifie_point(A,B,p,P):
+    res = math.sqrt(math.pow(P.x, A) + P.x + B)
+    return res == P.y
+
+
+
+P = Point(2, 1)
+
+print(verifie_point(3,2,3,P))
