03bc4c6fcacd27f47280b26962f3c5194bf27d88
SECHI Bahia <bahia.sechi@ecole.ensicaen.fr>
2020-12-03 09:40:27 +0100

    TP2 - Fonction verifie point OK

--- TP2_EllipticCurve/TP2.py
+++ TP2_EllipticCurve/TP2.py
@@ -5,6 +5,7 @@
 import math
 from Crypto.Util import number
 
+
 class Point:
     def __init__(self, x, y, z):
         self.x = x
@@ -21,12 +22,13 @@ class Point:
 
 def verifie_point(A, B, p, P):
     res = (math.pow(P.x, 3) + A * P.x + B)
-    #print(res, math.pow(P.y, 2))
-    return (res%p == (math.pow(P.y, 2)%p) or (P.z == 0))
+    # print(res, math.pow(P.y, 2))
+    return res % p == (math.pow(P.y, 2) % p) or (P.z == 0)
 
 
 def addition_points(A, B, p, P, Q):
     point_infini = Point(0, 0, 0)
+
     if P.x == 0 and P.y == 0:
         return Q.toString()
     if Q.x == 0 and Q.y == 0:
@@ -36,29 +38,28 @@ def addition_points(A, B, p, P, Q):
         if P.x == Q.x:
             return point_infini.toString()
         elif P.x != Q.x:
-            lamb = (Q.y - P.y)*number.inverse((Q.x - P.x), p)
-            X = (lamb ** 2) - P.x - Q.x
-            Y = lamb * (P.x - X) - P.y
+            lamb = ((Q.y - P.y) * number.inverse((Q.x - P.x), p)) % p
+            X = ((lamb ** 2) - P.x - Q.x) % p
+            Y = (lamb * (P.x - X) - P.y) % p
             return Point(X, Y, 1).toString()
     elif P == Q:
         if P.y == 0:
             return point_infini.toString()
         elif P.y != 0:
-            lamb = (3 * (P.x ** 2) + A) * number.inverse((2 * P.y), p)
-            X = (lamb ** 2) - (2 * P.x)
-            Y = (lamb * (P.x - X)) - P.y
+            lamb = ((3 * (P.x ** 2) + A) * number.inverse((2 * P.y), p)) % p
+            X = ((lamb ** 2) - (2 * P.x)) % p
+            Y = ((lamb * (P.x - X)) - P.y) % p
             return Point(X, Y, 1).toString()
 
-def groupe_des_points(A,B,p):
-    points = []
-    points.append(Point(0,0,0))
+
+def groupe_des_points(A, B, p):
+    points = [Point(0, 0, 0)]
     for x in range(p):
         for y in range(p):
-            P = Point(x,y,1)
+            P = Point(x, y, 1)
             appartient = verifie_point(A, B, p, P)
-            if appartient : points.append(P)
+            if appartient: points.append(P)
     print("Pour X^3 + {}X + {} de groupe Z{} il y a {} element(s)".format(A, B, p, len(points)))
-    
 
 
 def ordre_point(A, B, p, P):
@@ -111,7 +112,6 @@ print("{} + {} = {}".format(P.toString(), Q.toString(), addition_points(A, B, p,
 P = Point(1, 4, 1)
 Q = Point(1, 4, 1)
 print("{} + {} = {}".format(P.toString(), Q.toString(), addition_points(A, B, p, P, Q)))
-
 
 print("\nFonction groupe_des_points :")
 groupe_des_points(3, 2, 5)
