a9c9f92a14a9df25ad2685ff9d9b23c44f1efd00
SECHI Bahia <bahia.sechi@ecole.ensicaen.fr>
2020-12-07 08:36:00 +0100

    TP3 - Skeleton

--- TP3_EllipticCurveP256/TP3.py
+++ TP3_EllipticCurveP256/TP3.py
@@ -1,3 +1,27 @@
 # -*- coding: utf-8 -*-
 # PRUNIER Bastien
 # SECHI Bahia
+
+from Crypto.Hash import SHA256
+from Crypto.Util import number
+
+
+def test_hasse(n, p):
+    return
+
+
+def ecdh(A, B, p, P):
+    return
+
+
+def ecdsa(A, B, p, P, n, m, a):
+    return
+
+
+def ecdsa_verif(A, B, p, P, n, m, A1, sign):
+    return
+
+
+# BONUS
+def attack():
+    return
