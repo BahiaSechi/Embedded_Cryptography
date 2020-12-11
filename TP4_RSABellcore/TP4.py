280c4faedf1c25bfb650c829b9e71c7e9521b10b
SECHI Bahia <bahia.sechi@ecole.ensicaen.fr>
2020-12-11 16:31:26 +0100

    TP4 - Work on CRT generating and sign

--- TP4_RSABellcore/TP4.py
+++ TP4_RSABellcore/TP4.py
@@ -2,16 +2,26 @@
 # PRUNIER Bastien
 # SECHI Bahia
 
+from Crypto.Util import number
+import math
+
 def generer_cle_RSA(n):
     return
 
 
-def generer_cle_RSA_CRT(n):
-    return
+def generer_cle_RSA_CRT(N, e, p, q, d):
+    dp = d % (p-1)
+    dq = d % (q-1)
+    ip = number.inverse(p, q) % q
+    iq = number.inverse(q, p) % p
+    return N, e, p, q, d, dp, dq, ip, iq
 
 
-def signature_RSA_CRT(m, sK):
-    return
+def signature_RSA_CRT(N, m, p, q, dp, dq):
+    sp = m*math.sqrt(dp) % p
+    sq = m*math.sqrt(dq) % q
+    s = (number.inverse(q, p)*q*sp + number.inverse(p, q) * p * sq) % N
+    return s
 
 
 def signature_RSA_CRT_faute(m, sK):
