0b5db20eb441162d0227c1053b29c6e9d4f05ec3
SECHI Bahia <bahia.sechi@ecole.ensicaen.fr>
2020-12-11 14:27:40 +0100

    TP4 - Skeleton

--- TP4_RSABellcore/TP4.py
+++ TP4_RSABellcore/TP4.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
 # PRUNIER Bastien
 # SECHI Bahia
+
+def generer_cle_RSA(n):
+    return
+
+
+def generer_cle_RSA_CRT(n):
+    return
+
+
+def signature_RSA_CRT(m, sK):
+    return
+
+
+def signature_RSA_CRT_faute(m, sK):
+    return
+
+
+def RSA_CRT_Bellcore(m, sK, pK):
+    return
